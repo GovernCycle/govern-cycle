d5aab1e0bea245a5df9fc790a1fce899cf126b96
Jaime Andres Muñoz <jaime.munozq@udea.edu.co>
2024-08-09 18:07:18 -0500

    Initial project structure

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+
+import { Client, InternetIdentity } from "@bundly/ares-core";
+import { IcpConnectContextProvider } from "@bundly/ares-react";
+
+import { candidCanisters } from "@app/canisters";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const client = Client.create({
+    agentConfig: {
+      host: process.env.NEXT_PUBLIC_IC_HOST_URL!,
+    },
+    candidCanisters,
+    providers: [
+      new InternetIdentity({
+        providerUrl:"https://identity.ic0.app/",
+      }),
+    ],
+  });
+
+  return (
+    <IcpConnectContextProvider client={client}>
+      <Component {...pageProps} />
+    </IcpConnectContextProvider>
+  );
+}
