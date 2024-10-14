ceffd0fd2e5bb1c79145bcca14715ecb1c830aee
niabnn <estefania.goez@udea.edu.co>
2024-10-14 18:00:53 -0500

    refactor: :art: Remove unused code and update dashboard header
    - Removed the unused file `proposal-detail/index.tsx` as it is no longer needed.
    - Updated the `dashboard/index.tsx` file to include the new `DashboardHeader` component.
    - Updated the `ubicationDropdown.tsx` file to include a placeholder prop for the dropdown component.
    - Deleted the `auth/prove/index.tsx` file as it is no longer needed.
    - Added the new `DashboardHeader` component to the `dashboard/index.tsx` file.

--- frontend/src/components/forms/ubicationDropdown.tsx
+++ frontend/src/components/forms/ubicationDropdown.tsx
@@ -4,19 +4,20 @@ import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 interface UbicationDropdownProps {
   options: string[];
+  placeholder: string; 
 }
 
-const UbicationDropdown: React.FC<UbicationDropdownProps> = ({ options }) => {
+const UbicationDropdown: React.FC<UbicationDropdownProps> = ({ options, placeholder }) => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
   };
-
+  
   return (
     <Menu as="div" className="relative">
       <MenuButton className="group flex items-center justify-between h-full px-3 py-2.5 text-sm font-semibold text-text-tertiary outline-none drop-shadow-[-4px_-4px_6px_rgba(255, 254, 239,0.2)] duration-200 ease-in-out hover:text-charcoal-500">
-        <span>{selectedOption || 'Ubicación'}</span>
+        <span>{selectedOption || placeholder}</span> {/* Esto o puse para mostrar el placeholder si no se ha seleccionado ninguna opción */}
         <ChevronDownIcon className="ml-1 h-4.5 w-4.5 text-text-tertiary duration-300 group-data-[open]:rotate-180" />
       </MenuButton>
 
@@ -29,7 +30,7 @@ const UbicationDropdown: React.FC<UbicationDropdownProps> = ({ options }) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute z-50 mt-4 w-full rounded-md bg-tan-500/70 shadow-lg"> {/* Ajusta el margen aquí */}
+        <Menu.Items className="absolute z-50 mt-4 w-full rounded-md bg-tan-500/70 shadow-lg">
           {options.map((option) => (
             <Menu.Item key={option}>
               {({ active }) => (
