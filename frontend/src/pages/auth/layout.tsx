bfd8da1b5bb4a3c755913d58bc307c8a2a1f735c
Rony-san <ronysantiagoba@gmail.com>
2024-10-14 03:03:51 -0500

    style: :lipstick: New FormLayout

--- frontend/src/pages/auth/layout.tsx
+++ frontend/src/pages/auth/layout.tsx
@@ -1,27 +1,27 @@
 import Image from 'next/image'
 import { HeroContainer } from '@/components/shared/HeroContainer'
 import { Button } from '@/components/shared/Button'
-import { Constellation } from '@/components/auth/Constellation'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
-// import spaceSpotlight from '@/images/space-spotlight.png'
 import spaceSpotlight from '@/images/space-spotlight.png'
 import forestLignthing from '@/images/forest-light-background.png'
-import forestSingUp from '@/images/forest-sing-up.webp'
+import FortestLeft from '@/images/forest-green-left.png'
+import FortestRight from '@/images/forest-green-right.png'
+import birdsLeft from '@/images/birds1.png'
+import birdsRight from '@/images/birds2.png'
+import cloudLeft from '@/images/Cloud1.png'
+import cloudRight from '@/images/Cloud2.png'
 
 export default function AuthLayout({
   children,
-
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <HeroContainer
       header={false}
       starField={true}
       starfieldWhite={true}
       form={true}
-      className='flex min-h-screen items-center justify-center overflow-hidden py-16 sm:py-20'
+      className='flex min-h-screen items-center justify-center overflow-hidden '
       bgGradientClassName='-top-48 bottom-0 h-[calc(100%_+_320px)]'
       innerContainerClassName='w-full overflow-hidden sm:overflow-visible'
     >
@@ -29,23 +29,65 @@ export default function AuthLayout({
         <Image
           src={forestLignthing}
           alt=''
-          className='w-full h-auto object-cover scale-150 '
+          className='w-full h-auto object-cover custom-scale'
           sizes='(max-width: 1024px) 100vw, 1024px'
           priority
         />
       </div>
-      <div className='absolute bottom-0 left-0 right-0 z-3'>
+
+      <div className="absolute bottom-0 left-0 z-3 animate-slide-in-left">
         <Image
-          src={forestSingUp}
-          alt=''
-          className='w-full h-auto object-cover'
-          sizes='(max-width: 1024px) 100vw, 1024px'
+          src={FortestLeft}
+          alt=""
+          className="h-auto w-[60vw] object-cover lg:w-[60vw]"
+          sizes="(max-width: 1024px) 60vw, 512px"
           priority
         />
       </div>
 
+      <div className="absolute bottom-0 right-0 z-4 animate-slide-in-right ">
+        <Image
+          src={FortestRight}
+          alt=""
+          className="h-auto w-[70vw] object-cover lg:w-[70vw]"
+          sizes="(max-width: 1024px) 60vw, 512px"
+          priority
+        />
+      </div>
 
-      <Constellation />
+      {/* Cambiar `fixed` por `absolute` */}
+      <div className="absolute top-10 left-[20%] z-5 animate-combined-left">
+        <Image
+          src={birdsLeft}
+          alt=""
+          className="h-auto w-[20vw] object-cover"
+        />
+      </div>
+
+      <div className="absolute top-10 right-[20%] z-5 animate-combined-right">
+        <Image
+          src={birdsRight}
+          alt=""
+          className="h-auto w-[20vw] object-cover"
+        />
+      </div>
+      <div className="absolute top-10 left-[5%] z-5 animate-cloud-left">
+        <Image
+          src={cloudLeft}
+          alt=""
+          className="h-auto w-[20vw] object-cover"
+        />
+      </div>
+
+      <div className="absolute top-96 right-[20%] z-5 animate-cloud-right">
+        <Image
+          src={cloudRight}
+          alt=""
+          className="h-auto w-[10vw] object-cover"
+        />
+      </div>
+
+
       <Button
         href='/'
         variant='secondary'
