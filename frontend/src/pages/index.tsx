5c60742cc82a9a048adee84892bd6dc752fde695
Rony-san <ronysantiagoba@gmail.com>
2024-10-22 18:01:58 -0500

    style: :art: Implementing the eventGadget

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { HomeHero } from '@/components/home/HomeHero'
 import { Divider } from '@/components/shared/Divider'
 import { Features } from '@/components/home/Features'
 import { Footer } from '@/components/shared/Footer'
+import { InvestorTestimonials } from '@app/components/pricing/InvestorTestimonials'
 
 export default function IcConnectPage() {
   return (
@@ -12,6 +13,8 @@ export default function IcConnectPage() {
       </HeroContainer>
       <Divider />
       <Features />
+      <Divider />
+      <InvestorTestimonials />
       <Divider />
       <Footer />
     </>
